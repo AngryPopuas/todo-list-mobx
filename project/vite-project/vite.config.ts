b9b0fc83af9bdf36b0c8207bd8a193d73b188441
Denis <alaxababa@mail.ru>
2024-08-30 14:51:30 +0500

    project finished

--- project/vite-project/vite.config.ts
+++ project/vite-project/vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
+  base: "/deploying-tasks",
   plugins: [react()],
   resolve: {
     alias: {
